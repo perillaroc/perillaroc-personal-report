e1c5c2e0b57ee91916389a68e826345e2a1e514c
perillaroc <perillaroc@gmail.com>
2018-01-17 23:25:51 +0800

    add a simple leaflet map.

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,9 @@
 
 $(document).ready(function() {
+
+  // var my_map = L.map('thailand-map').setView([98.297843, 7.898293], 13);
+
+
   $('#fullpage').fullpage({
     sectionsColor: [
       'whitesmoke',
@@ -12,4 +16,12 @@ $(document).ready(function() {
     ],
     scrollOverflow: true
   });
+
+  var mymap = L.map('thailand-map').setView([7.898293, 98.297843], 17);
+  L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 18,
+    attribution: 'Map data <a target="_blank" href="http://www.openstreetmap.org">OpenStreetMap.org</a>' +
+    ' contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+  }).addTo(mymap);
+
 });
